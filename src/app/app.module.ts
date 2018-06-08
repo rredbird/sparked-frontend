8975abe941de7a3578b5628d7ec612d3eba2b67e
beuthdev\rui <robin.ruth@beuth.de>
2018-06-08 14:21:06 +0200

    properties page created,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 import { BackendService } from './backend-service/backend.service';
 import { HttpClient } from 'selenium-webdriver/http';
+import { PropertiesComponent } from './properties/properties.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppNavbarComponent
+    AppNavbarComponent,
+    PropertiesComponent
   ],
   imports: [
     BrowserModule,
